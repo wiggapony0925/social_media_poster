aa8edadffd2d1110a9562e99d7054d5832d15deb
wiggapony0925 <ninjeff06@gmail.com>
2023-10-29 01:41:48 -0400

    passes firsrt test for twitter

---
+++ test/test_twitter_api.py
@@ -0,0 +1,26 @@
+import unittest
+from unittest.mock import MagicMock
+from social_api import TwitterApi
+
+class TestTwitterApi(unittest.TestCase):
+
+    def test_post(self):
+        # Mock the Tweepy API
+        mock_api = MagicMock()
+        twitter_api = TwitterApi(
+            client_key='client_key',
+            client_secret='client_secret',
+            access_token='access_token',
+            access_token_secret='access_token_secret'
+        )
+        twitter_api.api = mock_api  # Override the api attribute with your mock object
+
+        # Call the post method
+        content = "Test tweet content"
+        twitter_api.post(content)
+
+        # Check that the mock API's update_status method was called with the correct argument
+        mock_api.update_status.assert_called_once_with(status=content)
+
+if __name__ == '__main__':
+    unittest.main()
