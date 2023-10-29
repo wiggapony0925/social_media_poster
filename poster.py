de35b890d0db48725b0fe5ba20f7251351b88e1f
wiggapony0925 <ninjeff06@gmail.com>
2023-10-29 12:36:15 -0400

    layout of twitter api

--- poster.py
+++ poster.py
@@ -1,7 +1,21 @@
 from social_api import TwitterApi
 from dotenv import load_dotenv
+import yaml
 import os
 
+def get_message(file_path):
+    try:
+            with open(file_path, 'r') as file:
+                data = yaml.load(file, Loader=yaml.FullLoader)
+            return data
+    except FileNotFoundError:
+         print(f"YAML file not found: {file_path}")
+         return {}
+    except Exception as e:
+        print(f"Error loading YAML file: {str(e)}")
+        return {}
+    
+    
 def poster():
     load_dotenv()
     
@@ -14,10 +28,16 @@ def poster():
     }
     
     twitter_api = TwitterApi(**twitter_credentials)
-    
-    # Create a post
     content = "Hello, world! This is a test post."
     twitter_api.post(content)
+    
+    
+    #FACEBOOk 
+    
+
+    
 
 if __name__ == '__main__':
+    data = get_message('config.yaml') 
+    print(data)
     poster()
