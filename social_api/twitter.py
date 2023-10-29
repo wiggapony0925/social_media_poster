e7249a16cf67f7af6e8ef234a895bcd996410a3e
wiggapony0925 <ninjeff06@gmail.com>
2023-10-29 00:59:49 -0400

    sample twitter api using classes

--- social_api/twitter.py
+++ social_api/twitter.py
@@ -1,2 +1,10 @@
 import tweepy
 
+class TwitterApi:
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
+        self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        self.auth.set_access_token(access_token, access_token_secret)
+        self.api = tweepy.API(self.auth)
+        
+    def post(self, content):
+        self.api.update_status(status=content)
